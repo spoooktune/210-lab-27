54fc670ab90d07116fa75dc0de2eb3cc906c0d38
spoooktune <asriprasana5@gmail.com>
2025-11-02 20:39:03 -0800

    added main_menu func and coded some functionality for menu

--- main.cpp
+++ main.cpp
@@ -3,22 +3,72 @@
 #include <vector>
 using namespace std;
 
+int main_menu(){
+    int option = 0;
+    while (option != 1 && option != 2 && option != 3 && option != 4){
+        cout << "1. Increase Friendship" << endl;
+        cout << "2. Decrease Friendship" << endl;
+        cout << "3. Search For Villager" << endl;
+        cout << "4. Exit" << endl << endl;
+        cout << "Choose an option: ";
+        cin >> option;
+    }
+    return option;
+}
+
 int main() {
     // declarations
-    map<string, tuple<int, string, string>> villagers;
     // key: villager name, data: friendship level, species, catchprase
-
-    // insert elements into the map
-    // note how the right-hand side of the assignment are the vector elements
+    map<string, tuple<int, string, string>> villagers;
     villagers["Apollo"] = {7, "Eagle", "pah"};
     villagers["Carmen"] = {10, "Rabbit", "nougat"};
     villagers.insert({"Fang", {5, "Wolf", "cha-chomp"}});
 
+    int option = main_menu();
+    while (option != 4){
+        switch (option){
+            case 1:
+                for (auto pair : villagers){
+                    get<0>(pair.second)++;
+                }
+                break;
+            case 2:
+                for (auto pair : villagers){
+                    get<0>(pair.second)--;
+                }
+                break;
+            case 3:
+                string name;
+                cout << "Enter villager name: ";
+                cin >> name;
+                auto it = villagers.find(name);
+                if (it != villagers.end()) {  
+                    cout << name << "'s data" << endl;
+                    auto [friendship, species, catchphrase] = it->second;
+                    cout << "> Friendship Level: " << friendship << endl;
+                    cout << "> Species: " << species << endl;
+                    cout << "> Catchphrase: " << catchphrase << endl;
+                } 
+                else
+                    cout << endl << name << " not found." << endl;
+                cout << endl;
+                break;
+        }
+        cout << "Villager Data: " << endl;
+        for (auto pair : villagers) {
+            cout << pair.first << " [";
+            cout << get<0>(pair.second) << ", " << get<1>(pair.second) << ", " << get<2>(pair.second) << "]" << endl;
+        }
+        cout << endl;
+        option = main_menu();
+    }
+    /*
     // access the map using a range-based for loop
     cout << "Villager Data (range-based for loop):" << endl;
     for (auto pair : villagers) {
         cout << pair.first << " [";
-        cout << get<0>(pair.second) << ", " << get<1>(pair.second) << ", " << get<2>(pair.second) << "]" << endl;
+        auto [friendship, species, catchphrase] = it->second;
+        cout << "[" << friendship << ", " << species << ", " << catchphrase << "]" << endl;
     }
     cout << endl;
 
@@ -39,22 +89,12 @@ int main() {
     // search for an element using .find() to avoid errors
     
     string searchKey = "Fang";
-    auto it = villagers.find(searchKey);
-    if (it != villagers.end()) {  // the iterator points to beyond the end of the map
-                                       // if searchKey is not found
-        cout << "\nFound " << searchKey << "'s data" << endl;
-        auto [friendship, species, catchphrase] = it->second;
-        cout << "Friendship Level: " << friendship << endl;
-        cout << "Species: " << species << endl;
-        cout << "Catchphrase: " << catchphrase << endl;
-    } else
-        cout << endl << searchKey << " not found." << endl;
-    cout << endl;
+    
 
     // report size, clear, report size again to confirm map operations
     cout << "Size before clear: " << villagers.size() << endl;
     villagers.clear();
     cout << "Size after clear: " << villagers.size() << endl;
-
+    */
     return 0;
 }
